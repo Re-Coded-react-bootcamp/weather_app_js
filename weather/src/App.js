0f7c1d2c61a7ba433a27bc147cd479eed9024d81
Murtaja Adnan <murtaja07805@gmail.com>
2021-02-10 22:53:57 +0300

    display city info fixes #8

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-
+import "./App.css"
+import WeatherData from "./containers/WeatherData/WeatherData"
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<WeatherData />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- weather/src/containers/WeatherData/WeatherData.jsx
+++ weather/src/containers/WeatherData/WeatherData.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react"
+import CityInfo from "../../components/ProgressBar/CityInfo"
+import axios from "axios"
 
 export default function WeatherData() {
 	const [currentWeth, setcurrentWeth] = useState()
@@ -14,10 +15,10 @@ export default function WeatherData() {
 				axios
 					.all([
 						axios.get(
-							'http://api.weatherstack.com/current?access_key=306bc2ca601a9eb579319d8b96507b0d&query=San Francisco'
+							"http://api.weatherstack.com/current?access_key=306bc2ca601a9eb579319d8b96507b0d&query=San Francisco"
 						),
 						axios.get(
-							'https://tranquil-cove-12072.herokuapp.com/https://www.metaweather.com/api/location/2487956/'
+							"https://tranquil-cove-12072.herokuapp.com/https://www.metaweather.com/api/location/2487956/"
 						)
 					])
 					.then(([currentRes, foreCastRes]) => {
@@ -34,5 +35,10 @@ export default function WeatherData() {
 	if (!currentWeth) {
 		return null
 	}
-	return <div>{/* components come here */}</div>
+	return (
+		<div>
+			{/* components come here */}
+			<CityInfo currentWeth={currentWeth} />
+		</div>
+	)
 }
