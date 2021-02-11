7170e4f5ceb6905c3a3978656a9b685dd69e1c49
kodo-yousif <60068120+kodo-yousif@users.noreply.github.com>
2021-02-11 14:32:16 +0300

    all search style updated

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,14 @@
-import React, { Component } from "react";
 import "./SearchBar.css";
-
-export default class SearchBar extends Component {
-  render() {
-    return (
-      <div className="SearchBar">
-        <div className="SearchChilds">
-          <h5 className="text-white">Search weather on your city</h5>
-          <input
-            type="text"
-            name="city"
-            id="inputSearchCity"
-            onChange={this.onChange}
-            placeholder="Search"
-          />
-        </div>
+import Option from "../option/Option";
+export default function SearchBar() {
+  let x = <div className="smallld">Loading...</div>;
+  return (
+    <div className="SearchBar">
+      <div className="SearchChilds">
+        <h5 className="text-white">Search weather on your city</h5>
+        <input type="text" autoComplete="false" placeholder="Search" />
+        <div className="options"></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
---
+++ src/components/option/Option.jsx
@@ -0,0 +1,4 @@
+import "./Option.css";
+export default function Option(props) {
+  return <div className="option">{props.city}</div>;
+}
